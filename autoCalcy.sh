d17196a22f8cc49eb4a3190cafa517ec894cbf8a
AliceGrey <alice@grey.systems>
2021-01-11 10:28:53 -0600

    Add support for autoscan
    Script now supports autoscan for even faster renaming

--- autoCalcy.sh
+++ autoCalcy.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 #Simple shell script that taps the screen to rename pokemon with their IV using the CalcyIV Android App
 
-#Calcy button coordinates
+#-SETTINGS-#
+#Set to 1 to enable appraising pokemon
+#This is slower but will give a more accurate IV
+appraise=1
+#Set to 1 if you use autoscan in calcy
+#This will skip the calcy button pressing step
+autoscan=1
+
+#-COORDINATES-#
+#Menu button coordinates - only needed if you want to use appraise
+menu_x=940
+menu_y=2050
+#Appraise button coordinates - only needed if you want to use appraise
+appraise_x=935
+appraise_y=1650
+#Calcy button coordinates - not needed if you use autoscan
 calcy_y=900
 calcy_x=990
 #Center of the calcy popup dialog coordinates
 center_x=540
-center_y=1300
+center_y=1350
 #Pokemons name coordinates
 name_x=550
 name_y=1000
@@ -22,20 +37,9 @@ pogoOK_y=1200
 #Space below the AR button that moves to the next pokemon
 next_x=1010
 next_y=680
-#Counter - don't change
+
+#Counter - don't change this one
 n=1
-
-
-#Set to 1 to enable appraising pokemon
-#This is slower but will give a more accurate IV
-appraise=1
-#Menu button coordinates
-menu_x=940
-menu_y=2050
-#Appraise button coordinates
-appraise_x=935
-appraise_y=1650
-
 
 
 #Change 10 to be however many pokemon you want to scan/rename
@@ -57,6 +61,8 @@ do
     sleep .5
     fi
     
+    if [ $autoscan = 0 ]
+    then
     echo "Tapping Calcy"
     adb shell input tap $calcy_x $calcy_y
     sleep .5
@@ -64,12 +70,13 @@ do
     echo "Dismissing Calcy"
     adb shell input tap $center_x $center_y
     sleep .5
+    fi
     
     if [ $appraise = 1 ]
     then
     echo "Skipping dialog 2"
     adb shell input tap $center_x $center_y
-    sleep .5
+    sleep 1
     fi
     
     echo "Tapping Name"
@@ -93,7 +100,7 @@ do
 
     echo "----Next Pokemon----"
     adb shell input tap $next_x $next_y
-    sleep .5
+    sleep 1
 
     #Increment counter
     n=$((n+1))
